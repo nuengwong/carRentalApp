c419a70846dfc84f1f889beb3dbf1b3ad3dea48f
Ohm <nuengwong.t@cp.eng.chula.ac.th>
2024-03-25 16:57:47 +0700

    Chapter08 done

--- src/components/CarPanel.tsx
+++ src/components/CarPanel.tsx
@@ -1,9 +1,12 @@
 'use client';
-import { useReducer } from 'react';
+import { useReducer, useRef } from 'react';
 import ProductCard from './ProductCard';
 import Link from 'next/link';
 
 export default function CarPanel() {
+  const countRef = useRef(0);
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const compareReducer = (
     compareList: Set<string>,
     action: { type: string; carName: string }
@@ -70,6 +73,33 @@ export default function CarPanel() {
           {car}
         </div>
       ))}
+
+      <button
+        className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 shadow-sm text-white"
+        onClick={() => {
+          countRef.current = countRef.current + 1;
+          alert(countRef.current);
+        }}
+      >
+        Count with Ref
+      </button>
+      <input
+        type="text"
+        placeholder="Please fill"
+        className="block text-gray-900 text-sm rounded-lg p-2 m-2 bg-purple-50 ring-1 ring-inset ring-purple-400
+      focus:outline-none focus:bg-purple-200 fodus:ring-2"
+        ref={inputRef}
+      />
+      <button
+        className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 shadow-sm text-white"
+        onClick={() => {
+          if (inputRef.current != null) {
+            inputRef.current.focus();
+          }
+        }}
+      >
+        Focus Input
+      </button>
     </div>
   );
 }
---
+++ src/components/VlogPlayer.tsx
@@ -0,0 +1,30 @@
+import { useRef, useEffect, useState } from 'react';
+import { useWindowListener } from '@/hooks/useWindowListener';
+export default function VlogPlayer({
+  vdoSrc,
+  isPlaying,
+}: {
+  vdoSrc: string;
+  isPlaying: boolean;
+}) {
+  const vdoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    //alert('width is ' + vdoRef.current?.videoWidth);
+    if (isPlaying) {
+      //alert('Play VDO');
+      vdoRef.current?.play();
+    } else {
+      //alert('Pause VDO');
+      vdoRef.current?.pause();
+    }
+  }, [isPlaying]);
+
+  useWindowListener('resize', (e) => {
+    alert('window width is ' + (e.target as Window).innerWidth);
+  });
+
+  return (
+    <video className="w-[40%]" src={vdoSrc} ref={vdoRef} controls loop muted />
+  );
+}
